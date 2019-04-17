1e979ec1febe99b50e87054069e90af02873f80f
Alexandre Vanini <vanini.alexandre@gmail.com>
2019-04-17 13:38:01 +0200

    [TESTS] - JAVALIN
    Premier test de javalin sur une application HelloWorld

---
+++ javalin_test_dojo_hepia/src/main/java/HelloWorld.java
@@ -0,0 +1,8 @@
+import io.javalin.Javalin;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        Javalin app = Javalin.create().start(7000);
+        app.get("/", ctx -> ctx.result("Hello World"));
+    }
+}
